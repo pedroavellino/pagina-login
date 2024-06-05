efcbfb3aa381f3f6508c543a2f7c5bf56ffa4af4
Pedro Avelino de Almeida <pedroavelino@MacBook-Air-de-Pedro-2.local>
2024-06-04 21:36:19 -0300

    Desenvolvimento da primeira versão do projeto

---
+++ assets/javascript/script.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Formulário enviado!");
+  });
+});
